69746ec8d008ff0d8a955986f6cd44e9552c7970
Arkady G <gude@list.ru>
2020-12-12 18:54:48 +0300

    Практикум к модулю 15 вариант-01

--- Word_ search.py
+++ Word_ search.py
@@ -1,4 +1,4 @@
-alpha_ru = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ-'
+alpha_ru = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 alpha_en = 'abcdefghiklmnopqrstvxyzABCDEFGHIKLMNOPQRSTVXYZ'
 
 
@@ -18,7 +18,7 @@ def normalization(text):
 
 def definition_ru(text):
     output_ru = []
-    for word in text:  # определение слов с заданным количеством букв и добавление этих слов в новый список
+    for word in text:  # определение слов на русском языке с заданным количеством букв и добавление в список
         if 10 > len(word) > 5:
             for char in word:
                 if char in alpha_ru and word not in output_ru:
@@ -28,22 +28,22 @@ def definition_ru(text):
 
 
 def main():
-    MyFile = open(r'C:\Users\RIENZA\Основы Python\file_input_ru_en.txt', 'r', encoding='UTF8')  # открытие файла
-    words = MyFile.read()  # чтение текста
+    MyFile_01 = open(r'C:\Users\RIENZA\Основы Python\file_input_ru_en.txt', 'r', encoding='UTF8')  # открытие файла
+    words = MyFile_01.read()  # чтение текста
     words = normalization(words)  # удаление лишних знаков и создание предварительного списка
-    words = definition_ru(words)
+    words_ru = definition_ru(words)
 
     # print(words)
 
     MyFile_02 = open('output.txt', 'w', encoding='UTF8')
-    # # MyFile_02.write(str(output))  # запись в файл списком
+    MyFile_02.write('Список слов на русском языке с количеством букв более 5 и менее 10: \n\n')
+    # MyFile_02.write(str(output_ru))  # запись в файл списком
 
-    for element in words:  # запись в файл (построчно, столбиком)
-        MyFile_02.write(element)
+    for word_ru in words_ru:  # запись в файл (построчно, столбиком)
+        MyFile_02.write(word_ru)
         MyFile_02.write('\n')
     MyFile_02.close()
-
-    MyFile.close()
+    MyFile_01.close()
 
 
 if __name__ == '__main__':
